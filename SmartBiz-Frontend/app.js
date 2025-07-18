423d9ad06846ddc74bd8c93d61b84ac48562cab1
CodzTitan <codztitan18@gmail.com>
2025-07-18 15:56:00 +0530

    Add files via upload

---
+++ SmartBiz-Frontend/app.js
@@ -0,0 +1,11 @@
+const API_URL = "https://smartbiz-webapi.onrender.com";  // replace with your Render backend URL
+
+document.getElementById('loadProducts').addEventListener('click', () => {
+    fetch(`${API_URL}/products`)
+        .then(response => response.json())
+        .then(data => {
+            const list = data.map(p => `<li>${p.name} - ₹${p.price}</li>`).join('');
+            document.getElementById('productList').innerHTML = `<ul>${list}</ul>`;
+        })
+        .catch(err => console.error('Error:', err));
+});
