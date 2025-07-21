a4fe56642f819d1d83fe6beb7bb08b01c1ab89f5
CodzTitan <codztitan18@gmail.com>
2025-07-21 15:57:37 +0530

    Update app.js

--- SmartBiz-Frontend/app.js
+++ SmartBiz-Frontend/app.js
@@ -1,7 +1,7 @@
-const API_URL = "https://smartbiz-webapi.onrender.com";  // replace with your Render backend URL
+const API_URL = https://smartbiz-webapi.onrender.com;  // replace with your Render backend URL
 
 document.getElementById('loadProducts').addEventListener('click', () => {
-    fetch({API_URL}/products`)
+    fetch('${API_URL}/products`)
         .then(response => response.json())
         .then(data => {
             const list = data.map(p => `<li>${p.name} - ₹${p.price}</li>`).join('');
